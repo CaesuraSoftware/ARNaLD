a62b5194b03bcd02598f5508a84d7993ac4393ef
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-09 02:16:19 -0400

    StateAtom update

--- Caesura.Arnald.Core/Agents/StateAtom.cs
+++ Caesura.Arnald.Core/Agents/StateAtom.cs
@@ -5,11 +5,13 @@ namespace Caesura.Arnald.Core.Agents
 {
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading.Tasks;
     
     public class StateAtom
     {
         // TODO: define one state here (and how it connects with other states)
         public String Name { get; set; }
+        public Action<Task> Action { get; set; }
         private List<StateAtom> ComesAfterState { get; set; }
         
         public StateAtom()
@@ -17,13 +19,14 @@ namespace Caesura.Arnald.Core.Agents
             this.ComesAfterState = new List<StateAtom>();
         }
         
-        public void AddPreceedingState(StateAtom atom)
+        public Boolean AddPreceedingState(StateAtom atom)
         {
             if (this.ComesAfterState.Exists(x => x.Name == atom.Name || x.Name == this.Name))
             {
-                return;
+                return false;
             }
             this.ComesAfterState.Add(atom);
+            return true;
         }
         
         public Boolean ComesAfter(String name)
