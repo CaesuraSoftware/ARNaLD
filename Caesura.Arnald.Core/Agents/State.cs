48f9259f7ecb31198a431e9d871cb78e5efe736a
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-09 01:41:58 -0400

    Working on state machine for agents

---
+++ Caesura.Arnald.Core/Agents/State.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace Caesura.Arnald.Core.Agents
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    public class State : IState
+    {
+        // TODO: state machine for agents to act based off message context (got a command, waiting for message, etc.)
+    }
+}
