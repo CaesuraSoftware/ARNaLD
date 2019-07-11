30b859544ce77e6ed354a51032b01c630eb309ab
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-11 09:07:21 -0400

    Locator is disposable

--- Caesura.Arnald.Core/Agents/Locator.cs
+++ Caesura.Arnald.Core/Agents/Locator.cs
@@ -7,23 +7,33 @@ namespace Caesura.Arnald.Core.Agents
     using System.Linq;
     using Caesura.Standard;
     
-    public class Locator
+    public class Locator : IDisposable
     {
         public String Name { get; set; }
         public Guid Identifier { get; set; }
+        public Boolean DisposeAgentsOnDispose { get; set; }
         private List<IAgent> Agents { get; set; }
         public event Action<Locator, IAgent> OnAdd;
         public event Action<Locator, IAgent> OnRemove;
+        public event Action<Locator, IAgent> OnDisposeAgent;
+        public event Action<Locator> OnDispose;
         
         public Locator()
         {
             this.Agents = new List<IAgent>();
             this.Identifier = Guid.NewGuid();
+            this.DisposeAgentsOnDispose = true;
         }
         
-        public Locator(String name) : this()
+        public Locator(String name, Boolean disposeAgentsOnDispose) : this()
         {
             this.Name = name;
+            this.DisposeAgentsOnDispose = disposeAgentsOnDispose;
+        }
+        
+        public Locator(String name) : this(name, true)
+        {
+            
         }
         
         public Maybe<IAgent> Find(Predicate<IAgent> predicate)
@@ -86,6 +96,19 @@ namespace Caesura.Arnald.Core.Agents
         public Boolean Remove(IAgent agent)
         {
             return this.Remove(agent.Name);
+        }
+        
+        public void Dispose()
+        {
+            this.OnDispose.Invoke(this);
+            if (this.DisposeAgentsOnDispose)
+            {
+                foreach (var agent in this.Agents)
+                {
+                    this.OnDisposeAgent(this, agent);
+                    agent.Dispose();
+                }
+            }
         }
     }
 }
