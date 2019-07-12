0bd18399aed72186b1f1f3b5751e091e9d031bac
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-12 02:51:41 -0400

    work on starting and stopping agents

--- Caesura.Arnald.Core/Agents/BaseAgent.cs
+++ Caesura.Arnald.Core/Agents/BaseAgent.cs
@@ -5,6 +5,7 @@ namespace Caesura.Arnald.Core.Agents
 {
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     
     /// <summary>
@@ -18,16 +19,40 @@ namespace Caesura.Arnald.Core.Agents
         public String Name { get; protected set; }
         public Guid Identifier { get; protected set; }
         public IPersonality Personality { get; protected set; }
+        public ThreadState AgentThreadState { get; protected set; }
         protected IMailbox Messages { get; set; }
         protected IState AgentState { get; set; }
         
         public BaseAgent()
         {
-            this.Identifier     = Guid.NewGuid();
-            this.Name           = this.Identifier.ToString("N").ToUpper();
-            this.Personality    = new Personality();
-            this.Messages       = new Mailbox();
-            this.AgentState     = State.LoadDefaults(this);
+            this.Identifier         = Guid.NewGuid();
+            this.Name               = this.Identifier.ToString("N").ToUpper();
+            this.AgentThreadState   = ThreadState.Unstarted;
+            this.Personality        = new Personality();
+            this.Messages           = new Mailbox();
+            this.AgentState         = State.LoadDefaults(this);
+        }
+        
+        public virtual void Start()
+        {
+            this.AgentThreadState = ThreadState.Running;
+            // TODO: create a long-running task here, store the cancellationtoken in this class
+            // call Execute in a loop
+            throw new NotImplementedException();
+        }
+        
+        public virtual void Stop()
+        {
+            this.AgentThreadState = ThreadState.StopRequested;
+            // TODO: call the cancellationtoken's cancel here
+            throw new NotImplementedException();
+        }
+        
+        public virtual void StopAndWait()
+        {
+            this.Stop();
+            this.AgentThreadState = ThreadState.WaitSleepJoin;
+            throw new NotImplementedException();
         }
         
         public virtual void Execute()
@@ -58,6 +83,7 @@ namespace Caesura.Arnald.Core.Agents
         {
             this.AgentState?.Dispose();
             this.Personality?.Dispose();
+            this.StopAndWait();
         }
     }
 }
--- Caesura.Arnald.Core/Agents/IAgent.cs
+++ Caesura.Arnald.Core/Agents/IAgent.cs
@@ -12,6 +12,10 @@ namespace Caesura.Arnald.Core.Agents
         String Name { get; }
         Guid Identifier { get; }
         IPersonality Personality { get; }
+        ThreadState AgentThreadState { get; }
+        void Start();
+        void Stop();
+        void StopAndWait();
         void Execute();
         void Learn(IBehavior behavior);
         void Send(IMessage message);
