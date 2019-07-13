5777f7b6445c5f905e7517342fd0d255cdea9752
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-13 17:37:12 -0400

    Resolver improvements

--- Caesura.Arnald.Core/Agents/IMessageResolver.cs
+++ Caesura.Arnald.Core/Agents/IMessageResolver.cs
@@ -9,6 +9,7 @@ namespace Caesura.Arnald.Core.Agents
         String Name { get; set; }
         IMessageHandler HostHandler { get; set; }
         IMessage Current { get; set; }
+        Boolean CheckIfRecipientIsHostName { get; set; }
         MessageResolverResult Check(IMessage message);
         void Execute();
     }
--- Caesura.Arnald.Core/Agents/MessageResolver.cs
+++ Caesura.Arnald.Core/Agents/MessageResolver.cs
@@ -13,37 +13,46 @@ namespace Caesura.Arnald.Core.Agents
         public String Name { get; set; }
         public IMessageHandler HostHandler { get; set; }
         public IMessage Current { get; set; }
+        public Boolean CheckIfRecipientIsHostName { get; set; }
         public State ResolverState { get; set; }
         public CheckCallback CheckCallback { get; set; }
         public ExecuteCallback ExecuteCallback { get; set; }
         
         public MessageResolver()
         {
-            this.ResolverState      = new State();
-            this.ExecuteCallback    = (resolver) => this.ResolverState.Next(this.Current);
+            this.CheckIfRecipientIsHostName     = true;
+            this.ResolverState                  = new State();
+            this.ExecuteCallback                = (resolver) => this.ResolverState.Next(this.Current);
         }
         
         public MessageResolver(String name) : this()
         {
-            this.Name               = name;
+            this.Name = name;
         }
         
         public MessageResolver(String name, CheckCallback checker) : this(name)
         {
-            this.CheckCallback      = checker;
+            this.CheckCallback = checker;
         }
         
         public MessageResolver(String name, CheckCallback checker, ExecuteCallback execute) : this(name, checker)
         {
-            this.ExecuteCallback    = execute;
+            this.ExecuteCallback = execute;
         }
         
         public MessageResolverResult Check(IMessage message)
         {
             this.Current = message;
+            if (this.CheckIfRecipientIsHostName)
+            {
+                if (message.Recipient != this.HostHandler.HostAgent.Name)
+                {
+                    return MessageResolverResult.Pass;
+                }
+            }
             if (this.CheckCallback is null)
             {
-                return MessageResolverResult.None;
+                return MessageResolverResult.Pass;
             }
             return this.CheckCallback.Invoke(this, message);
         }
