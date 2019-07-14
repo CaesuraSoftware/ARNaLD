f403d91017c51e307caca2d2cd3e7e4808770115
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-14 15:24:46 -0400

    more monitor work

--- Tools/Debugging/Caesura.PerformanceMonitor/Caesura.PerformanceMonitor/Monitor/MonitorResult.cs
+++ Tools/Debugging/Caesura.PerformanceMonitor/Caesura.PerformanceMonitor/Monitor/MonitorResult.cs
@@ -8,6 +8,26 @@ namespace Caesura.PerformanceMonitor.Monitor
     
     public class MonitorResult
     {
+        public Double ProcessorUsage { get; set; }
+        public String ProcessorUsagePercent => String.Format("{0:0.0}", this.ProcessorUsage);
+        public Int64 MemoryBytesUsed { get; set; }
+        public Int64 MemoryMegabytesUsed => this.MemoryBytesUsed / 1024 / 1024;
+        public Int32 ClrThreads { get; set; }
+        public Int32 ThreadCount => this.Threads?.Count ?? 0;
+        public List<MonitorThread> Threads { get; set; }
         
+        public MonitorResult()
+        {
+            this.Threads = new List<MonitorThread>();
+        }
+        
+        public class MonitorThread
+        {
+            public Int32 ThreadId { get; set; }
+            public Int32 BasePriority { get; set; }
+            public Int32 CurrentPriority { get; set; }
+            public Double ProcessorUsage { get; set; }
+            public String ProcessorUsagePercent => String.Format("{0:0.0}", this.ProcessorUsage);
+        }
     }
 }
--- Tools/Debugging/Caesura.PerformanceMonitor/Caesura.PerformanceMonitor/Monitor/Windows.cs
+++ Tools/Debugging/Caesura.PerformanceMonitor/Caesura.PerformanceMonitor/Monitor/Windows.cs
@@ -5,6 +5,7 @@ namespace Caesura.PerformanceMonitor.Monitor
 {
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading;
     using System.Diagnostics;
     
     public class Windows : IMonitor
@@ -12,38 +13,104 @@ namespace Caesura.PerformanceMonitor.Monitor
         public Boolean ProcessIsAlive => !this.TargetProcess.HasExited;
         public String Name => this.TargetProcess.ProcessName;
         private Process TargetProcess { get; set; }
-        private PerformanceCounter CpuCounter { get; set; }
-        private PerformanceCounter RamCounter { get; set; }
-        private PerformanceCounter ThreadCounter { get; set; }
+        private Boolean Started { get; set; }
+        private TimeSpan InitialTime { get; set; }
+        private TimeSpan PreviousTime { get; set; }
+        private DateTime LastMonitorTime { get; set; }
+        private DateTime StartTime { get; set; }
+        private List<ThreadTimeTracker> ThreadTimes { get; set; }
         
         public Windows()
         {
-            
+            this.Started            = false;
+            this.PreviousTime       = new TimeSpan(0);
+            this.LastMonitorTime    = DateTime.UtcNow;
+            this.StartTime          = DateTime.UtcNow;
+            this.ThreadTimes        = new List<ThreadTimeTracker>();
         }
         
-        public Windows(Int32 pid)
+        public Windows(Int32 pid) : this()
         {
             this.TargetProcess = Process.GetProcessById(pid);
-            
-            // TODO: this returns Time * CPU Cores, meaning a process taking up 100% on a 4 core machine would return 400%
-            this.CpuCounter = new PerformanceCounter("Process", "% Processor Time", this.TargetProcess.ProcessName);
-            // TODO: display with (ram/1024/1024) for megabytes
-            this.RamCounter = new PerformanceCounter("Process", "Working Set", this.TargetProcess.ProcessName);
-            this.ThreadCounter = new PerformanceCounter("Process", "Thread Count", this.TargetProcess.ProcessName);
         }
         
         // TODO: method that calls all NextValue()'s for all PerformanceCounters and puts them all in some POCO
         // TODO: foreach (ProcessThread pt in p.Threads) pt.TotalProcessorTime
         public MonitorResult GetStatus()
         {
-            throw new NotImplementedException();
+            if (!this.Started)
+            {
+                this.InitialTime = this.TargetProcess.TotalProcessorTime;
+                this.Started = true;
+            }
+            
+            var currentTime = this.TargetProcess.TotalProcessorTime - this.InitialTime;
+            
+            var result = new MonitorResult();
+            
+            result.ProcessorUsage = (
+                (currentTime - this.PreviousTime).TotalSeconds / 
+                (Environment.ProcessorCount * DateTime.UtcNow.Subtract(this.LastMonitorTime).TotalSeconds)
+            );
+            
+            result.MemoryBytesUsed = this.TargetProcess.WorkingSet64;
+            
+            var upthreads = this.TargetProcess.Threads;
+            var threads = new List<ProcessThread>(upthreads.Count);
+            foreach (ProcessThread thread in upthreads)
+            {
+                threads.Add(thread);
+            }
+            foreach (var thread in threads)
+            {
+                if (!this.ThreadTimes.Exists(x => x.ThreadId == thread.Id))
+                {
+                    var ttt             = new ThreadTimeTracker();
+                    ttt.ThreadId        = thread.Id;
+                    ttt.InitialTime     = thread.TotalProcessorTime;
+                    this.ThreadTimes.Add(ttt);
+                }
+                
+                var tracker             = this.ThreadTimes.Find(x => x.ThreadId == thread.Id);
+                var currentThreadTime   = thread.TotalProcessorTime - tracker.InitialTime;
+                
+                var mt                  = new MonitorResult.MonitorThread();
+                mt.ThreadId             = thread.Id;
+                mt.BasePriority         = thread.BasePriority;
+                mt.CurrentPriority      = thread.CurrentPriority;
+                mt.ProcessorUsage       = (
+                        (currentTime - tracker.PreviousTime).TotalSeconds / 
+                        DateTime.UtcNow.Subtract(this.LastMonitorTime).TotalSeconds
+                    );
+                
+                tracker.PreviousTime    = currentThreadTime;
+                result.Threads.Add(mt);
+            }
+            // remove all threads that have exited
+            this.ThreadTimes.RemoveAll(x => threads.Any(y => x.ThreadId != y.Id));
+            
+            this.LastMonitorTime = DateTime.UtcNow;
+            this.PreviousTime = currentTime;
+            
+            return result;
         }
         
         public void Dispose()
         {
-            this.CpuCounter?.Dispose();
-            this.RamCounter?.Dispose();
-            this.ThreadCounter?.Dispose();
+            
+        }
+        
+        internal class ThreadTimeTracker
+        {
+            public Int32 ThreadId { get; set; }
+            public TimeSpan InitialTime { get; set; }
+            public TimeSpan PreviousTime { get; set; }
+            
+            public ThreadTimeTracker()
+            {
+                this.InitialTime  = new TimeSpan(0);
+                this.PreviousTime = new TimeSpan(0);
+            }
         }
     }
 }
