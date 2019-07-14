eaeb58caf26553d401023633e842d4c2d5135aca
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-14 12:29:35 -0400

    todo

--- Caesura.Arnald.Service/Program.cs
+++ Caesura.Arnald.Service/Program.cs
@@ -4,7 +4,12 @@ namespace Caesura.Arnald.Service
 {
     class Program
     {
-        static void Main(string[] args)
+        // TODO: create a new project Caesura.Arnald.Monitor that acts as
+        // a simple frontend, starts Caesura.Arnald.Service and then implements
+        // resource tracking features to both display the service performance
+        // and provides console I/O, probably through some fancy split-screen
+        // console program.
+        static void Main(String[] args)
         {
             Console.WriteLine("Hello World!");
         }
