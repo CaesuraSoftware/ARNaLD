8603e9478dbf3d0a754a0503f3174e59528a9a1c
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-14 10:57:10 -0400

    todo

--- Tools/Debugging/Caesura.PerformanceMonitor/Caesura.PerformanceMonitor/Program.cs
+++ Tools/Debugging/Caesura.PerformanceMonitor/Caesura.PerformanceMonitor/Program.cs
@@ -4,6 +4,13 @@ namespace Caesura.PerformanceMonitor
 {
     class Program
     {
+        // TODO:
+        //  - seperate monitoring and displaying info
+        //  - make a nice console display, not just printing in a loop
+        //  - check args for: 
+        //    - processid (default: itself, for debugging purposes)
+        //    - update interval (default: 1000ms)
+        //    - shutdown after process ends (default: true)
         static void Main(String[] args)
         {
             Console.WriteLine("Hello World!");
