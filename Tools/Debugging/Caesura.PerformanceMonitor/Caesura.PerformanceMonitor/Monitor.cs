17faad6660ad3bc67f4d30963153e6ed2bbde839
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-14 10:53:59 -0400

    Beginning work on performance monitor tool

---
+++ Tools/Debugging/Caesura.PerformanceMonitor/Caesura.PerformanceMonitor/Monitor.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace Caesura.PerformanceMonitor
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    public class Monitor
+    {
+        
+    }
+}
---
+++ Tools/Debugging/Caesura.PerformanceMonitor/Caesura.PerformanceMonitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Caesura.PerformanceMonitor
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
