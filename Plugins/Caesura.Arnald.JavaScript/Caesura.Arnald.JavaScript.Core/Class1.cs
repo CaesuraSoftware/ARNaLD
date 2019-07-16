74ce1b6a85314f344e1a0f9f150a0d27c15f66d0
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-16 12:22:59 -0400

    todo (ClearScript isn't an option right now)

--- Plugins/Caesura.Arnald.JavaScript/Caesura.Arnald.JavaScript.Core/Class1.cs
+++ Plugins/Caesura.Arnald.JavaScript/Caesura.Arnald.JavaScript.Core/Class1.cs
@@ -3,9 +3,12 @@
 namespace Caesura.Arnald.JavaScript.Core
 {
     // TODO: 
-    // - Implement and deploy Microsoft.ClearScript (V8)
-    //   https://github.com/microsoft/ClearScript
-    //   https://www.nuget.org/packages/JavaScriptEngineSwitcher.V8.Native.win-x64/
+    // - Implement and deploy V8:
+    //   https://github.com/rjamesnw/v8dotnet
+    //   Unfortunately we can't use Microsoft.ClearScript because it's Windows-specific
+    //   and according to them would be a large undertaking to make portable.
+    // - Add optional TypeScript mode:
+    //   https://stackoverflow.com/questions/19023498/loading-the-typescript-compiler-into-clearscript-wscript-is-undefined-imposs
     // 
     
     public class Class1
