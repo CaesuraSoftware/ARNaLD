07c36aa45157c62784196528744484f8ea820f65
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-16 11:56:52 -0400

    todo

--- Plugins/Caesura.Arnald.JavaScript/Caesura.Arnald.JavaScript.Core/Class1.cs
+++ Plugins/Caesura.Arnald.JavaScript/Caesura.Arnald.JavaScript.Core/Class1.cs
@@ -2,6 +2,12 @@
 
 namespace Caesura.Arnald.JavaScript.Core
 {
+    // TODO: 
+    // - Implement and deploy Microsoft.ClearScript (V8)
+    //   https://github.com/microsoft/ClearScript
+    //   https://www.nuget.org/packages/JavaScriptEngineSwitcher.V8.Native.win-x64/
+    // 
+    
     public class Class1
     {
     }
