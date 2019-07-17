84b48df6e60f30063cbc8cebf2d91a4224b44047
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-17 13:08:01 -0400

    todo

--- Caesura.Arnald.Core/Plugin/Communication/BaseCommunicationPlugin.cs
+++ Caesura.Arnald.Core/Plugin/Communication/BaseCommunicationPlugin.cs
@@ -3,6 +3,11 @@ using System;
 
 namespace Caesura.Arnald.Core.Plugin.Communication
 {
+    // TODO: either here or in a custom CommunicationMessage,
+    // create an array of Name/Value POCOs called "Contexts",
+    // a context is where the location came from or is going
+    // to, along with it's sub-locations. A Discord message
+    // might be [guild, channel, (user if dm)]
     
     public abstract class BaseCommunicationPlugin : BasePlugin
     {
