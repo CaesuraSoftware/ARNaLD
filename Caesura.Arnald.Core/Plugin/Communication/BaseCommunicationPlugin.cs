ca2a2ce6b7c6907aaa69c36e951a3b0a4550a71a
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-17 17:03:27 -0400

    todo

--- Caesura.Arnald.Core/Plugin/Communication/BaseCommunicationPlugin.cs
+++ Caesura.Arnald.Core/Plugin/Communication/BaseCommunicationPlugin.cs
@@ -8,6 +8,7 @@ namespace Caesura.Arnald.Core.Plugin.Communication
     // a context is where the location came from or is going
     // to, along with it's sub-locations. A Discord message
     // might be [guild, channel, (user if dm)]
+    // TODO: need to support streaming as well as posting.
     
     public abstract class BaseCommunicationPlugin : BasePlugin
     {
