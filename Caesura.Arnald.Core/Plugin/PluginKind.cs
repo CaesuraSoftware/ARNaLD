cba5d26385a9b1f4581098fdf584bb53ccc72487
Kameko Kirishiki <kameko.k@outlook.com>
2019-07-17 11:12:09 -0400

    PluginKind addition

--- Caesura.Arnald.Core/Plugin/PluginKind.cs
+++ Caesura.Arnald.Core/Plugin/PluginKind.cs
@@ -3,12 +3,14 @@ using System;
 
 namespace Caesura.Arnald.Core.Plugin
 {
+    [Flags]
     public enum PluginKind : Int32
     {
-        None            = 1 << 0,
-        General         = 1 << 1,
-        Behavior        = 1 << 2,
-        Scripting       = 1 << 3,
-        Communication   = 1 << 4,
+        None            =      0,
+        General         = 1 << 0,
+        Behavior        = 1 << 1,
+        Scripting       = 1 << 2,
+        Communication   = 1 << 3,
+        Storage         = 1 << 4,
     }
 }
