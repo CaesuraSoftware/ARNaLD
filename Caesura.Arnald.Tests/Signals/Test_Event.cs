b24a8a88e3829d1dd2f9e4f020364a166a87326f
Kameko Kirishiki <kameko.k@outlook.com>
2019-08-19 13:05:25 -0400

    Event testing

--- Caesura.Arnald.Tests/Signals/Test_Event.cs
+++ Caesura.Arnald.Tests/Signals/Test_Event.cs
@@ -13,20 +13,94 @@ namespace Caesura.Arnald.Tests.Signals
     {
         public Test_Event(ITestOutputHelper output) : base(output)
         {
-            this.AddTest(nameof(SampleTest_1), this.SampleTest_1);
+            this.AddTest(nameof(SubscriptionTest_1) , this.SubscriptionTest_1);
+            this.AddTest(nameof(RaiseTest_1)        , this.RaiseTest_1);
+            this.AddTest(nameof(BlockTest_1)        , this.BlockTest_1);
         }
         
         [Fact]
-        public void SampleTest_1()
+        public void SubscriptionTest_1()
         {
             // Setup and constants
-            Boolean a = false;
+            String name = "event0"; // no relation to the video game
+            IEvent ev = new Event(name);
+            Boolean activated1 = false;
+            Boolean disposed1 = false;
+            ActivatorCallback onactivate1 = (self, signal) =>
+            {
+                activated1 = true;
+            };
+            Action<IActivator> onunsub1 = (self) =>
+            {
+                disposed1 = true;
+            };
             
             // Test proper
-            a = true;
+            IActivator sub1 = ev.Subscribe(onactivate1);
+            sub1.OnUnsubscribe = onunsub1;
+            sub1.Raise();
+            sub1.Unsubscribe();
             
             // Assertions
-            Assert.True(a);
+            Assert.True(activated1);
+            Assert.True(disposed1);
+        }
+        
+        [Fact]
+        public void RaiseTest_1()
+        {
+            // Setup and constants
+            String name = "event0";
+            IEvent ev = new Event(name);
+            Boolean activated1 = false;
+            ActivatorCallback onactivate1 = (self, signal) =>
+            {
+                activated1 = true;
+            };
+            Boolean activated2 = false;
+            ActivatorCallback onactivate2 = (self, signal) =>
+            {
+                activated2 = true;
+            };
+            
+            // Test proper
+            IActivator sub1 = ev.Subscribe(onactivate1);
+            IActivator sub2 = ev.Subscribe(onactivate2);
+            sub1.Raise();
+            
+            // Assertions
+            Assert.NotEqual(sub1.Name, sub2.Name);
+            Assert.True(activated1);
+            Assert.True(activated2);
+        }
+        
+        [Fact]
+        public void BlockTest_1()
+        {
+            // Setup and constants
+            String name = "event0";
+            IEvent ev = new Event(name);
+            Boolean activated1 = false;
+            ActivatorCallback onactivate1 = (self, signal) =>
+            {
+                activated1 = true;
+            };
+            Boolean activated2 = false;
+            ActivatorCallback onactivate2 = (self, signal) =>
+            {
+                activated2 = true;
+            };
+            
+            // Test proper
+            IActivator sub1 = ev.Subscribe(onactivate1);
+            IActivator sub2 = ev.Subscribe(onactivate2);
+            sub1.Block();
+            sub1.Raise();
+            
+            // Assertions
+            Assert.NotEqual(sub1.Name, sub2.Name);
+            Assert.True(activated1);
+            Assert.False(activated2);
         }
     }
 }
