ae09aadecf62a1a8c5366190707f7e5513b47ee3
Kameko Kirishiki <kameko.k@outlook.com>
2019-08-19 11:27:36 -0400

    ForEach/Map improvements

--- Caesura.Standard/Caesura.Standard/ForEach.cs
+++ Caesura.Standard/Caesura.Standard/ForEach.cs
@@ -8,7 +8,7 @@ namespace Caesura.Standard
     using System.Threading.Tasks;
     
     [Flags]
-    public enum ForEachResult : Int32
+    public enum MapResult : Int32
     {
         None        = 1 << 0,
         Success     = 1 << 1,
@@ -16,34 +16,49 @@ namespace Caesura.Standard
         Halt        = 1 << 3,
     }
     
-    public delegate ForEachResult ForEachDelegate<T>(T element);
-    public delegate ForEachResult ForEachIteratorDelegate<T>(T element, ref Int32 iterator);
+    public delegate MapResult MapCallback<T>(T element);
+    public delegate MapResult MapIteratorCallback<T>(T element, ref Int32 iterator);
     
     public static class ForEachExtension
     {
-        public static void ForEach<T>(this IEnumerable<T> collection, ForEachDelegate<T> callback)
+        public static void Map<T>(this IEnumerable<T> collection, Action<T> callback)
         {
-            // TODO: change these to GetEnumerator?
-            var size = collection.Count();
-            for (var i = 0; i < size; i++)
+            collection.Map<T>(item =>
             {
-                var e = collection.ElementAt(i);
-                var result = callback.Invoke(e);
-                if (result.HasFlag(ForEachResult.Failure | ForEachResult.Halt))
+                callback.Invoke(item);
+                return MapResult.Success;
+            });
+        }
+        
+        public static void Map<T>(this IEnumerable<T> collection, MapCallback<T> callback)
+        {
+            var enumerator = collection.GetEnumerator();
+            try
+            {
+                while (enumerator.MoveNext())
                 {
-                    break;
+                    var current = enumerator.Current;
+                    var result = callback.Invoke(current);
+                    if (result.HasFlag(MapResult.Failure | MapResult.Halt))
+                    {
+                        break;
+                    }
                 }
+            }
+            finally
+            {
+                enumerator?.Dispose();
             }
         }
         
-        public static void ForEach<T>(this IEnumerable<T> collection, ForEachIteratorDelegate<T> callback)
+        public static void Map<T>(this IEnumerable<T> collection, MapIteratorCallback<T> callback)
         {
             var size = collection.Count();
             for (var i = 0; i < size; i++)
             {
                 var e = collection.ElementAt(i);
                 var result = callback.Invoke(e, ref i);
-                if (result.HasFlag(ForEachResult.Failure | ForEachResult.Halt))
+                if (result.HasFlag(MapResult.Failure | MapResult.Halt))
                 {
                     break;
                 }
